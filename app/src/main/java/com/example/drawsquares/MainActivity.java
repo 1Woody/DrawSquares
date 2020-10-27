5e616fce368d2779cc6585a031e7fbdadef66c48
[daniel.benavente4@gmail.com] <[daniel.benavente4@gmail.com]>
2020-10-27 21:12:49 +0100

    circle,square,triangle

--- app/src/main/java/com/example/drawsquares/MainActivity.java
+++ app/src/main/java/com/example/drawsquares/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.drawsquares;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
@@ -20,22 +24,84 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageview;
     Bitmap bitmap;
     Canvas canvas;
-    Paint paintfill,paintstroke, paintcircle;
-    Button bt;
+    Paint paintfill,paintstroke;
 
     int screenwidth, screenheight;
     int width,height, squaresize;
 
+    private class ButtonMenu{
+        Button bt;
+        public ButtonMenu(final String text, Context THIS){
+            bt = new Button(THIS);
+            bt.setText(text);
+            bt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int x1 = (int)(Math.random()*(width-squaresize));
+                    int y1 = (int)(Math.random()*(height-squaresize));
+                    switch (text) {
+                        case "SQUARE":
+                            paintfill.setColor(Color.BLUE);
+                            paintstroke.setColor(Color.YELLOW);
+                            drawRect(x1, y1, x1 + squaresize, y1 + squaresize);
+                            break;
+                        case "CIRCLE":
+                            paintfill.setColor(Color.BLACK);
+                            paintstroke.setColor(Color.GREEN);
+                            drawCircle(x1, y1);
+
+                            break;
+                        case "TRIANGLE":
+                            paintfill.setColor(Color.MAGENTA);
+                            paintstroke.setColor(Color.WHITE);
+                            drawTriangle(x1, y1);
+                            break;
+                        default:
+                            canvas.drawColor(Color.RED);
+                            break;
+                    }
+                }
+            });
+        }
+    }
+
     public void drawRect(int x1, int y1, int x2, int y2){
         canvas.drawRect(x1,y1,x2,y2,paintfill);
         canvas.drawRect(x1,y1,x2,y2,paintstroke);
-        //canvas.drawCircle(x1,y1,100,paintcircle);
+
+    }
+
+    public void drawCircle(int x1, int y1){
+
+        canvas.drawCircle(x1,y1,60,paintfill);
+        canvas.drawCircle(x1,y1,60,paintstroke);
+    }
+
+    public void drawTriangle(int x1, int y1){
+
+        Point a = new Point(x1,y1);
+        Point b = new Point(x1+squaresize, y1);
+        Point c = new Point(x1+squaresize/2, y1-squaresize);
+
+        Path path = new Path();
+        path.moveTo(a.x,a.y);
+        path.lineTo(a.x, a.y);
+        path.lineTo(b.x, b.y);
+        path.lineTo(c.x, c.y);
+        path.lineTo(a.x, a.y);
+        path.close();
+
+        canvas.drawPath(path, paintfill);
+        canvas.drawPath(path,paintstroke);
+
+
 
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String[] polygonNames = {"SQUARE","CIRCLE","TRIANGLE","CLEAN"};
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         screenwidth  = metrics.widthPixels;
@@ -49,31 +115,19 @@ public class MainActivity extends AppCompatActivity {
         bitmap = Bitmap.createBitmap(width,height, Bitmap.Config.ARGB_8888);
         imageview.setImageBitmap(bitmap);
         canvas = new Canvas(bitmap);
-        canvas.drawColor(Color.RED);
+
         paintfill = new Paint();
         paintfill.setColor(Color.BLUE);
         paintfill.setStyle(Paint.Style.FILL);
         paintstroke= new Paint();
-        paintstroke.setColor(Color.YELLOW);
         paintstroke.setStyle(Paint.Style.STROKE);
         paintstroke.setStrokeWidth(5);
-        paintcircle= new Paint();
-        paintcircle.setColor(Color.BLACK);
-        paintcircle.setStyle(Paint.Style.FILL);
-        paintcircle.setStrokeWidth(5);
-        drawRect(width/2-squaresize/2, height/2-squaresize/2,
-                width/2+squaresize/2,height/2+squaresize/2);
-        bt = new Button(this);
-        bt.setText("New Square");
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int x1 = (int)(Math.random()*(width-squaresize));
-                int y1 = (int)(Math.random()*(height-squaresize));
-                drawRect(x1,y1,x1+squaresize,y1+squaresize);
-            }
-        });
-        linlay.addView(bt);
+
+        canvas.drawColor(Color.RED);
+        for(int i=0; i<4; i++){
+            linlay.addView((new ButtonMenu(polygonNames[i],this)).bt);
+        }
+
         linlay.setOrientation(LinearLayout.VERTICAL);
         linlay.setGravity(Gravity.CENTER_HORIZONTAL);
         setContentView(linlay);
